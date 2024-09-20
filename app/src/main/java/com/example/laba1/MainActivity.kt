0f0ad77ed1a8b8c953f915f53f51b128e92b64b3
LeraSimagina <lerasimagina025@gmail.com>
2024-09-20 23:22:57 +0700

    Метод checkAge

--- app/src/main/java/com/example/laba1/MainActivity.kt
+++ app/src/main/java/com/example/laba1/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.laba1
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -25,11 +26,25 @@ class MainActivity : AppCompatActivity() {
         result = findViewById(R.id.textView2)
 
 
+
         /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }*/
+    }
+
+    fun checkAge(view: View) {
+        val ageRes = age.text.toString().toIntOrNull()
+        result.text = when {
+            ageRes == null -> "Введите возраст!"
+            ageRes <= 20 -> "Вы слишком молоды!"
+            ageRes == 30 || ageRes == 40 || ageRes == 50 || ageRes == 60 -> "Поздравляем с повышением!"
+            ageRes == 65 -> "Преподносим вам золотые часы!"
+            ageRes > 65 -> "Вы слишком стар!"
+
+            else -> {"Продолжайте накапливать опыт!"}
+        }
     }
 
 }
