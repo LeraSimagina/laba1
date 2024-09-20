aa9e6d670d85977b677480714e707d3bdc05dee8
LeraSimagina <lerasimagina025@gmail.com>
2024-09-20 22:07:33 +0700

    Создание и инициализация переменных

--- app/src/main/java/com/example/laba1/MainActivity.kt
+++ app/src/main/java/com/example/laba1/MainActivity.kt
@@ -1,22 +1,35 @@
 package com.example.laba1
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var age: EditText
+    private lateinit var check: Button
+    private lateinit var result: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        age = findViewById(R.id.editTextText)
+        check = findViewById(R.id.button)
+        result = findViewById(R.id.textView2)
+
+
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
+        }*/
     }
-
 
 }
